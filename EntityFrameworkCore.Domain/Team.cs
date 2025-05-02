80c0506d1ee5b511b2e847245c0a4da357a49f7a
Owner <Owner@DESKTOP-BT3BM35>
2025-05-02 07:44:30 -0400

    Add project files.

---
+++ EntityFrameworkCore.Domain/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+    public class Team : BaseDomainModel
+    {
+        public int TeamId { get; set; }
+        public string? Name { get; set; }
+
+
+    }
+}
